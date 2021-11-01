92ae30e488bd30e5664847b5db78af8ecbc17446
patrick <patrick@o-neill.ca>
2021-11-01 12:47:45 -0400

    added reset button functionality

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-for (i = 0; i < 256; i++) {
-  const squareDivs = document.createElement('div')
-  squareDivs.classList.add('squareDiv')
-  grid.appendChild(squareDivs)
+// functions
+function setupGrid() {
+  for (i = 0; i < 256; i++) {
+    const squareDivs = document.createElement('div')
+    squareDivs.classList.add('squareDiv')
+    grid.appendChild(squareDivs)
+  }
 }
-
-const squareDivs = grid.querySelectorAll('div')
 
 function hover(e) {
   this.classList.add('hover')
   console.log(this.classList.value);
 }
 
+function resetGrid () {
+  grid.innerHTML = ''
+  setupGrid()
+  const squareDivs = grid.querySelectorAll('div')
+  squareDivs.forEach(div => div.addEventListener('mouseover', hover));
+}
+
+
+setupGrid()
+
+const squareDivs = grid.querySelectorAll('div')
+const resetBtn = document.getElementById('resetBtn')
+
 squareDivs.forEach(div => div.addEventListener('mouseover', hover));
+resetBtn.addEventListener('click', resetGrid);
+
